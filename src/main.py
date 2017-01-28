8f28996f42d97d88db1ba53003be98335e678ea4
Gasper Vozel <gasper.vozel@niteoweb.com>
2017-01-28 13:20:59 +0100

    Add analyse option which adds categories to payees.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
+from collections import defaultdict
 from cerberus import Validator
 from datetime import datetime, date
 
 import click
 import csv
+import yaml
 
 
 def _parse_payee(raw_payee: str) -> str:
@@ -36,6 +38,8 @@ def _parse_transaction_date(raw_transaction_date: str) -> date:
 )
 def parse(source, output):
     """Parse skb tbody table to .csv which can be used to inport to MMEX."""
+    with open('category_mapping.yaml', 'r') as stream:
+        category_mapping = yaml.load(stream)
     with open(source, 'r') as fp:
         with open(output, 'w', newline='') as csvfile:
             csv_writer = csv.writer(
@@ -48,6 +52,7 @@ def parse(source, output):
                 'payee': {'type': 'string'},
                 'amount': {'type': 'number', 'coerce': float},
                 'notes': {'type': 'string'},
+                # NOTE: you can choose date format in MMEX.
                 # 'transaction_date': {'type': 'date'},
                 'transaction_date': {'type': 'string'},
             }
@@ -57,9 +62,6 @@ def parse(source, output):
                     'payee': _parse_payee(transaction_info[0].contents[0].strip()),  # noqa
                     'amount': _parse_amount(transaction_info[1].text.strip()),
                     'notes': transaction_info[3].contents[0].strip(),
-                    # 'transaction_date': _parse_transaction_date(
-                    #     transaction_info[4].contents[0].strip()
-                    # ),
                     'transaction_date': transaction_info[4].contents[0].strip(),
                 })
                 if v.errors:
@@ -70,8 +72,66 @@ def parse(source, output):
                     v.document['amount'],
                     v.document['notes'],
                     v.document['transaction_date'],
+                    category_mapping.get(v.document['payee'], '')
                 ])
 
 
+@click.command()
+@click.option(
+    '--source',
+    default='example.html',
+    prompt='Input file',
+    help='Input source file.',
+)
+def analyse(source):
+    """Parse skb tbody table to .csv which can be used to inport to MMEX."""
+    data = defaultdict(int)
+
+    with open(source, 'r') as fp:
+        soup = BeautifulSoup(fp.read(), 'html.parser')
+        v = Validator()
+        v.schema = {
+            'payee': {'type': 'string'},
+        }
+        for transaction in soup.find_all('tr'):
+            transaction_info = transaction.find_all('td')
+            v.validate({
+                'payee': _parse_payee(transaction_info[0].contents[0].strip()),  # noqa
+            })
+            if v.errors:
+                raise Exception(v.errors)
+            data[v.document['payee']] += 1
+    with open('category_mapping.yaml', 'r') as stream:
+        existing_data = yaml.load(stream)
+    with open('category_mapping.yaml', 'w') as stream:
+        for payee, times in data.items():
+            if times >= 2:
+                # category = click.prompt(
+                #     '[*] Enter category for {}: '.format(payee),
+                #     type=str,
+                #     default=existing_data.get(payee),
+                # )
+                stream.write(
+                    yaml.dump(
+                        {payee: existing_data.get(payee, '<ENTER CATEGORY>')},
+                        default_flow_style=False),
+                    )
+
+
+@click.command()
+@click.option(
+    '--mode',
+    default='parse',
+    prompt='mode',
+    help='Program mode. It can be parse (default) or analyse.',
+    type=click.Choice(['parse', 'analyse']),
+)
+def run(mode):
+    if mode == 'parse':
+        parse()
+    elif mode == 'analyse':
+        analyse()
+
+
 if __name__ == '__main__':
-    parse()
+    run()
